a69f993fa5fe51465edc43e8bfe8ca23c4bde19d
mbuzzas <mbuzzas3@gmail.com>
2017-06-20 14:21:37 -0700

    adds home and react router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+import { Router, Route, Link } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const Root = () => {
-	return (
-		<Router>
-		<div className="App">
-        	<Route path='/' component={App} />
-      	</div>
-		</Router>
-	)
-}
+// const Root = () => {
+// 	return (
+// 		<Router>
+// 		<div className="App">
+//         	<Route path='/' component={App} />
+//       	</div>
+// 		</Router>
+// 	)
+// }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render((
+		<Router>
+        	<Route path='/' component={App} />
+		</Router>
+	), document.getElementById('root'));
 registerServiceWorker();
+
